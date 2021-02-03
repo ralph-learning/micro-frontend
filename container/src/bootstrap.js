1e355f98c36f7aa7384ae14a861fa15b4760cb2d
Ralph Effting <ralfting@gmail.com>
2021-02-03 15:49:58 -0300

    Create and export mount function Create a guard to avoid to try to set HTML when the el doesn't exist

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,6 +1,7 @@
-import 'products/ProductsIndex';
+import { mount } from 'products/ProductsIndex';
 import 'cart/CartShow';
 
 console.log('it is working')
 
+mount(document.querySelector('#my-products'))
 
--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -1,18 +1,25 @@
 import faker from 'faker';
 
 const mount = (el) => {
-  let products = "";
-  for(let i = 0; i < 3; i++) {
+  if(!el) {
+    console.error('The element does not exist.');
+    return;
+  }
+
+  let products = '';
+
+  for (let i = 0; i < 5; i++) {
     const name = faker.commerce.productName();
     products += `<div>${name}</div>`;
   }
 
   el.innerHTML = products;
-}
+};
 
-if(process.env.NODE_ENV) {
+if (process.env.NODE_ENV === 'development') {
   const el = document.querySelector('#dev-products');
-  if(el) {
+
+  if (el) {
     mount(el);
   }
 }
